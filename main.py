37d677c2eed006fde9693d2dfadc40b240e59c06
TrevorBrowning <trevryanbrowning@gmail.com>
2025-12-21 10:51:08 -0500

    feat: added player sprite / movement

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -20,7 +24,15 @@ def main():
                 return
     
         screen.fill("black")
+        updatable.update(dt)
+
+        for i in drawable:
+            i.draw(screen)
         pygame.display.flip()
+        
+        delta = clock.tick(60)
+        dt = delta / 1000
+        
 
 
 if __name__ == "__main__":
